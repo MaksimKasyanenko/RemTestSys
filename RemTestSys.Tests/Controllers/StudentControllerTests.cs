3bc3989113ce022037a9fa91a1e8442ca8cf540a
Максим Касьяненко <maksim93012@gmail.com>
2021-09-09 14:09:35 +0300

    Unit tests project is created

---
+++ RemTestSys.Tests/Controllers/StudentControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RemTestSys.Controllers;
+using RemTestSys.Domain.Interfaces;
+using RemTestSys.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RemTestSys.Tests.Controllers
+{
+    public class StudentController_LoginActionTests
+    {
+        [Fact]
+        public void WithoutParametersReturnsViewResult()
+        {
+            var controller = new StudentController(new Mock<IStudentService>().Object, new Mock<ISessionService>().Object);
+            Type expected = typeof(ViewResult);
+
+            var res = controller.Login();
+
+            Assert.IsType(expected, res);
+        }
+
+        [Fact]
+        public void ReturnsViewResultContainingEmptyLoginField_WhenPassedEmptyLoginField()
+        {
+            var controller = new StudentController(new Mock<IStudentService>().Object, new Mock<ISessionService>().Object);
+            Type expectedType = typeof(ViewResult);
+            var viewModel = new LoginViewModel { StudentLogId = "" };
+
+            var res = (ViewResult)(controller.Login(viewModel).Result);
+
+            Assert.IsType(expectedType, res);
+            Assert.Equal("", ((LoginViewModel)res.Model).StudentLogId);
+        }
+
+        [Fact]
+        public void ReturnViewResultContainingWrongLogIdInLoginFieldAnd1ModelError_WhenPassedWrongLogId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
