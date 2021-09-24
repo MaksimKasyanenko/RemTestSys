0969e9b4fc02afd06a68ab9d48a1ebee6716d1e1
Максим Касьяненко <maksim93012@gmail.com>
2021-09-24 17:05:30 +0300

    --

--- RemTestSys/Domain/Models/QuestionInSession.cs
+++ RemTestSys/Domain/Models/QuestionInSession.cs
@@ -3,6 +3,7 @@
     public class QuestionInSession
     {
         public int Id { get; set; }
+        public int SessionId { get; set; }
         public Session Session { get; set; }
         public Question Question { get; set; }
         public int SerialNumber { get; set; }
--- RemTestSys/ViewModel/AnswerViewModel.cs
+++ RemTestSys/ViewModel/AnswerViewModel.cs
@@ -2,7 +2,6 @@
 {
     public class AnswerViewModel
     {
-        public int SessionId { get; set; }
         public string[] Data { get; set; }
     }
 }
