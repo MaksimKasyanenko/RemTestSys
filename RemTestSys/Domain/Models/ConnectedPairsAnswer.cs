93d63e01ad18b43262fab2d927035eb7ec75d29e
MaksimKasyanenko <77959027+MaksimKasyanenko@users.noreply.github.com>
2021-10-07 18:14:32 +0300

    Add files via upload

--- RemTestSys/Domain/Models/ConnectedPairsAnswer.cs
+++ RemTestSys/Domain/Models/ConnectedPairsAnswer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
@@ -32,12 +32,32 @@ namespace RemTestSys.Domain.Models
 
         public override string[] GetAdditiveData()
         {
-            throw new NotImplementedException();
+            Pair[] pairs = JsonSerializer.Deserialize<Pair[]>(_serializedPairs);
+            string[] res = new string[pairs.Length*2];
+            RandomSequence rnd = new RandomSequence(0, pairs.Length);
+            for(int i=0; i<pairs.Length;i++){
+            	res[i*2]=pairs[i].Value1;
+            	res[i*2+1]=pairs[i].Value2;
+            }
+            return res;
         }
 
         public override bool IsMatch(string[] data)
         {
-            throw new NotImplementedException();
+            if(data==null)return false;
+            if(data.Length%2>0)return false;
+            Pair[] pairs = JsonSerializer.Deserialize<Pair[]>(_serializedPairs);
+            if(data.Length/2 != pairs.Length)return false;
+            
+            Pair[] inPairs = new Pair[pairs.Length];
+            for(int i=0;i<inPairs.Length;i++){
+            	inPairs[i]=new Pair(data[i*2], data[i*2+1]);
+            }
+            foreach(var pair in pairs){
+            	if(!inPairs.Any(p=>p.Value1==pair.Value1&&p.Value2==pair.Value2))
+            	    return false;
+            }
+            return true;
         }
 
         public struct Pair
