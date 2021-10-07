67895a1ed855d8c702ae1ba5defcae586eeb033d
Максим Касьяненко <maksim93012@gmail.com>
2021-10-07 14:52:27 +0300

    --

--- UnitTests/ConnectedPairsAnswerTests.cs
+++ UnitTests/ConnectedPairsAnswerTests.cs
@@ -17,58 +17,44 @@ namespace UnitTests
             public void SetMethodSetsSerilizedDataProperty()
             {
                 var answer = new ConnectedPairsAnswer();
+                var testData = GetTestAdditiveData();
 
-                answer.SetAdditiveData(new string[] {"a","b","c"}, new string[] {"A","B","C"});
+                answer.SetPairs(testData);
 
-                Assert.Equal("[\"a\",\"A\",\"b\",\"B\",\"c\",\"C\"]", answer.SerializedPairs);
+                Assert.Equal(JsonSerializer.Serialize(testData), answer.SerializedPairs);
             }
 
-            [Theory]
-            [InlineData(new string[] {}, new string[] {})]
-            [InlineData(new string[] {"a",null}, new string[] {"A","B"})]
-            [InlineData(new string[] {"a","b"}, new string[] {"A",null})]
-            [InlineData(new string[] {"a","b","c"}, new string[] {"A","B"})]
-            [InlineData(new string[] {"a","b"}, new string[] {"A","B","C"})]
-            [InlineData(new string[] {"a"}, null)]
-            [InlineData(null, new string[] { })]
-            public void SetMethodAndSerializedDataPropertyThrowInvalidOperationException_WhenPassedWrongData(string[]leftList, string[]rightList)
+            [Fact]
+            public void ThrowsArgumentNullException_WhenPassedNullToConstructorOfPairStruct()
+            {
+                Assert.Throws<ArgumentNullException>(()=>new ConnectedPairsAnswer.Pair("a",null));
+                Assert.Throws<ArgumentNullException>(()=>new ConnectedPairsAnswer.Pair(null,"b"));
+            }
+
+            [Fact]
+            public void SetMethodAndSerializedDataPropertyThrowInvalidOperationException_WhenPassedNull()
             {
                 var answer = new ConnectedPairsAnswer();
-                string[] temp = new string[leftList.Length+rightList.Length];
-                int tempIndex = 0;
-                for(int i = 0; i < leftList.Length; i++)
-                {
-                    temp[tempIndex++] = leftList[i];
-                    temp[tempIndex++] = rightList[i];
-                }
 
-                Assert.Throws<InvalidOperationException>(()=>answer.SetAdditiveData(leftList,rightList));
-                Assert.Throws<InvalidOperationException>(()=>answer.SerializedPairs=JsonSerializer.Serialize(temp));
+                Assert.Throws<InvalidOperationException>(()=>answer.SetPairs(null));
+            }
+            [Fact]
+            public void SetMethodAndSerializedDataPropertyThrowInvalidOperationException_WhenPassedLessThen1Pair()
+            {
+                var answer = new ConnectedPairsAnswer();
+
+                Assert.Throws<InvalidOperationException>(() => answer.SetPairs(new ConnectedPairsAnswer.Pair[0]));
             }
         }
 
         public class GettingAdditiveData
         {
-            [Theory]
-            [InlineData(new string[] {"t"},new string[] {"T"})]
-            [InlineData(new string[] {"tt","dd"}, new string[] {"TT","DD"})]
-            [InlineData(new string[] {"1","2","3"}, new string[] {"a","b","c"})]
-            public void ReturnsArrayAsLongAsLeftAndRightListBoth(string[] leftList, string[] rightList)
-            {
-                var answer = new ConnectedPairsAnswer();
-                answer.SetAdditiveData(leftList, rightList);
-                int expectLength = leftList.Length + rightList.Length;
-
-                string[] res = answer.GetAdditiveData();
-
-                Assert.Equal(expectLength, res.Length);
-            }
-
             [Fact]
             public void ReturnsStringArrayWhichContainsAllElementsFromSourceArraysInRightOrder()
             {
                 var answer = new ConnectedPairsAnswer();
-                answer.SetAdditiveData(new string[] {"a","b","c"},new string[] {"A","B","C"});
+                var testData = GetTestAdditiveData();
+                answer.SetPairs(testData);
 
                 var res = answer.GetAdditiveData();
 
@@ -89,7 +75,7 @@ namespace UnitTests
             public void ReturnsRightText()
             {
                 var answer = new ConnectedPairsAnswer();
-                answer.SetAdditiveData(new string[] {"a","b","c"},new string[] {"A","B","C"});
+                answer.SetPairs(GetTestAdditiveData());
 
                 string res = answer.RightText;
 
@@ -110,7 +96,7 @@ namespace UnitTests
             public void ReturnsFalse_WhenPassedWrongAnswerArray(params string[] answerArray)
             {
                 var answer = new ConnectedPairsAnswer();
-                answer.SetAdditiveData(new string[] {"a","b","c"}, new string[] {"A","B","C"});
+                answer.SetPairs(GetTestAdditiveData());
 
                 var res = answer.IsMatch(answerArray);
 
@@ -121,13 +107,22 @@ namespace UnitTests
             public void ReturnsTrue_WhenPassedRightAnswerArray()
             {
                 var answer = new ConnectedPairsAnswer();
-                answer.SetAdditiveData(new string[] {"a","b","c"},new string[] {"A","B","C"});
+                answer.SetPairs(GetTestAdditiveData());
                 string[] answerArray = new string[] {"a","A","b","B","c","C"};
 
                 bool res = answer.IsMatch(answerArray);
 
                 Assert.True(res);
             }
+        }
+
+        private static ConnectedPairsAnswer.Pair[] GetTestAdditiveData()
+        {
+            return new ConnectedPairsAnswer.Pair[] {
+                    new ConnectedPairsAnswer.Pair("a","A"),
+                    new ConnectedPairsAnswer.Pair("b","B"),
+                    new ConnectedPairsAnswer.Pair("c","C"),
+                };
         }
     }
 }
