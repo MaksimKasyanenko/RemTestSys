e43d4e7fda2766c828b2f19e84dc27fce27fd08f
MaksimKasyanenko <77959027+MaksimKasyanenko@users.noreply.github.com>
2021-10-17 19:08:03 +0300

    Update ResultsController.cs

--- RemTestSys/Areas/Editor/Controllers/ResultsController.cs
+++ RemTestSys/Areas/Editor/Controllers/ResultsController.cs
@@ -1,14 +1,16 @@
 namespace RemTestSys.Areas.Editor.Controllers
 {
     [Area("Editor")]
-    [Authorize(Role="Editor")]
+    [Authorize(Roles="Editor")]
     public class ResultsController : Controller
     {
-        public ResultsController()
+        public ResultsController(AppDbContext dbContext)
         {
-            
+            this.dbContext=dbContext??throw new ArgumentNullException(nameof(dbContext));
         }
-        public Task<IActionResult> Index()
+        private readonly AppDbContext dbContext;
+        [HttpGet]
+        public Task<IActionResult> Index(int page=1)
         {
             
         }
