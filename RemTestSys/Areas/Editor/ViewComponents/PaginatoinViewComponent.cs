bde385675b98856a121da5c5ede7a4366cc986a4
MaksimKasyanenko <77959027+MaksimKasyanenko@users.noreply.github.com>
2021-10-18 14:35:30 +0300

    Create PaginatoinViewComponent.cs

---
+++ RemTestSys/Areas/Editor/ViewComponents/PaginatoinViewComponent.cs
@@ -0,0 +1,10 @@
+namespace RemTestSys.Areas.Editor.ViewComponents
+{
+public class PaginationViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View();
+    }
+}
+}
