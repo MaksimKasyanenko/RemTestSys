3f6e79032f3bae1c0e983c38fef2ac156ee8eb87
MaksimKasyanenko <77959027+MaksimKasyanenko@users.noreply.github.com>
2021-10-18 14:47:09 +0300

    Update PaginatoinViewComponent.cs

--- RemTestSys/Areas/Editor/ViewComponents/PaginatoinViewComponent.cs
+++ RemTestSys/Areas/Editor/ViewComponents/PaginatoinViewComponent.cs
@@ -2,7 +2,7 @@ namespace RemTestSys.Areas.Editor.ViewComponents
 {
 public class PaginationViewComponent : ViewComponent
 {
-    public async Task<IViewComponentResult> InvokeAsync()
+    public async Task<IViewComponentResult> InvokeAsync(int countOfElements, int currentPage, int elementsPerPage)
     {
         return View();
     }
