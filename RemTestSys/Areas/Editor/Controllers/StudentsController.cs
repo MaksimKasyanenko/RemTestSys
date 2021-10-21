b82f948b5eb5d7c84d7d67019ca910edfe7f8efd
Максим Касьяненко <maksim93012@gmail.com>
2021-10-21 23:57:35 +0300

    created students controller and view for editor

--- RemTestSys/Areas/Editor/Controllers/StudentsController.cs
+++ RemTestSys/Areas/Editor/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -47,83 +48,6 @@ namespace RemTestSys.Areas.Editor.Controllers
             return View(student);
         }
 
-        // GET: Editor/Students/Create
-        public IActionResult Create()
-        {
-            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Id");
-            return View();
-        }
-
-        // POST: Editor/Students/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,LogId,FirstName,LastName,GroupId,RegistrationDate")] Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(student);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Id", student.GroupId);
-            return View(student);
-        }
-
-        // GET: Editor/Students/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var student = await _context.Students.FindAsync(id);
-            if (student == null)
-            {
-                return NotFound();
-            }
-            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Id", student.GroupId);
-            return View(student);
-        }
-
-        // POST: Editor/Students/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,LogId,FirstName,LastName,GroupId,RegistrationDate")] Student student)
-        {
-            if (id != student.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(student);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!StudentExists(student.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["GroupId"] = new SelectList(_context.Groups, "Id", "Id", student.GroupId);
-            return View(student);
-        }
-
         // GET: Editor/Students/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -152,11 +76,6 @@ namespace RemTestSys.Areas.Editor.Controllers
             _context.Students.Remove(student);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool StudentExists(int id)
-        {
-            return _context.Students.Any(e => e.Id == id);
         }
     }
 }
