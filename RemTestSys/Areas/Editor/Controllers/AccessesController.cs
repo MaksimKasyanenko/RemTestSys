ddd170b9a56d4afb930e917103aa284492b8440d
Максим Касьяненко <maksim93012@gmail.com>
2021-10-26 16:10:18 +0300

    --

--- RemTestSys/Areas/Editor/Controllers/AccessesController.cs
+++ RemTestSys/Areas/Editor/Controllers/AccessesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RemTestSys.Areas.Editor.ViewModel;
+using RemTestSys.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,7 @@ using System.Threading.Tasks;
 namespace RemTestSys.Areas.Editor.Controllers
 {
     [Area("Editor")]
-    [Authorize(Roles="Editor")]
+    [Authorize(Roles = "Editor")]
     public class AccessesController : Controller
     {
         public AccessesController(AppDbContext dbContext)
@@ -21,10 +22,70 @@ namespace RemTestSys.Areas.Editor.Controllers
         public async Task<IActionResult> Index()
         {
             AccessesViewModel accesses = new AccessesViewModel();
-            accesses.AccessesForAll = await dbContext.AccessesToTestForAll.Include(a=>a.Test).ToListAsync();
-            accesses.AccessesForGroups = await dbContext.AccessesToTestForGroup.Include(a => a.Test).Include(a=>a.Group).ToListAsync();
-            accesses.AccessesForStudents = await dbContext.AccessesToTestForStudent.Include(a=>a.Test).Include(a=>a.Student).ToListAsync();
+            accesses.AccessesForAll = await dbContext.AccessesToTestForAll.Include(a => a.Test).ToListAsync();
+            accesses.AccessesForGroups = await dbContext.AccessesToTestForGroup.Include(a => a.Test).Include(a => a.Group).ToListAsync();
+            accesses.AccessesForStudents = await dbContext.AccessesToTestForStudent.Include(a => a.Test).Include(a => a.Student).ToListAsync();
             return View(accesses);
+        }
+        public async Task<IActionResult> OpenAccessToTestForAll(int id)
+        {
+            Test test = await dbContext.Tests.FindAsync(id);
+            if (test != null && await dbContext.AccessesToTestForAll.AllAsync(a => a.TestId != test.Id))
+            {
+                dbContext.AccessesToTestForAll.Add(new AccessToTestForAll { Test = test });
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> OpenAccessToTestForGroups(int id)
+        {
+            ViewData["TestId"] = id;
+            var groups = await dbContext.Groups.ToListAsync();
+            return View(groups);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> OpenAccessToTestForGroups(int testId, int groupId)
+        {
+            var group = await dbContext.Groups.FindAsync(groupId);
+            var test = await dbContext.Tests.FindAsync(testId);
+            if(test != null && group != null && await dbContext.AccessesToTestForGroup.AllAsync(a=>a.TestId != testId || a.GroupId != groupId))
+            {
+                AccessToTestForGroup access = new AccessToTestForGroup
+                {
+                    GroupId = groupId,
+                    TestId = testId
+                };
+                dbContext.AccessesToTestForGroup.Add(access);
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OpenAccessToTestForStudent(int id)
+        {
+            ViewData["TestId"] = id;
+            var students = await dbContext.Students.ToListAsync();
+            return View(students);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> OpenAccessToTestForStudent(int testId, int studentId)
+        {
+            foreach (var id in ids)
+            {
+                if (await dbContext.AccessesToTestForStudent.AnyAsync(a => a.TestId == testId && a.StudentId == id)) continue;
+                AccessToTestForStudent access = new AccessToTestForStudent
+                {
+                    StudentId = id,
+                    TestId = testId
+                };
+                dbContext.AccessesToTestForStudent.Add(access);
+            }
+            await dbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
         }
         public async Task<IActionResult> ClearAll()
         {
@@ -37,19 +98,19 @@ namespace RemTestSys.Areas.Editor.Controllers
 
         public async Task<IActionResult> DeleteForAll(int id)
         {
-            dbContext.AccessesToTestForAll.Remove(dbContext.AccessesToTestForAll.Find(id));
+            dbContext.AccessesToTestForAll.Remove(await dbContext.AccessesToTestForAll.FindAsync(id));
             await dbContext.SaveChangesAsync();
             return RedirectToAction("Index");
         }
         public async Task<IActionResult> DeleteForGroups(int id)
         {
-            dbContext.AccessesToTestForGroup.Remove(dbContext.AccessesToTestForGroup.Find(id));
+            dbContext.AccessesToTestForGroup.Remove(await dbContext.AccessesToTestForGroup.FindAsync(id));
             await dbContext.SaveChangesAsync();
             return RedirectToAction("Index");
         }
         public async Task<IActionResult> DeleteForStudent(int id)
         {
-            dbContext.AccessesToTestForStudent.Remove(dbContext.AccessesToTestForStudent.Find(id));
+            dbContext.AccessesToTestForStudent.Remove(await dbContext.AccessesToTestForStudent.FindAsync(id));
             await dbContext.SaveChangesAsync();
             return RedirectToAction("Index");
         }
