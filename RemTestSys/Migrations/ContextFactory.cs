fd8f5fd66f8fd4cc6c79137a49952bf8b422cee4
Максим Касьяненко <maksim93012@gmail.com>
2021-11-08 15:09:04 +0200

    Create DbContextFactory

---
+++ RemTestSys/Migrations/ContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RemTestSys.Domain;
+
+namespace RemTestSys.Migrations
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            string connectionString = @"";
+            optionsBuilder.UseSqlServer(connectionString, opts => opts.CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds));
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
