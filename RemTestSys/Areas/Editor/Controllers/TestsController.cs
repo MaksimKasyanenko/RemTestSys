55bebd972b0f8dc8ba6a005b7befd71e2f8395dd
Максим Касьяненко <maksim93012@gmail.com>
2021-11-18 12:36:20 +0200

    --

--- RemTestSys/Areas/Editor/Controllers/TestsController.cs
+++ RemTestSys/Areas/Editor/Controllers/TestsController.cs
@@ -56,9 +56,6 @@ namespace RemTestSys.Areas.Editor.Controllers
             return View();
         }
 
-        // POST: Editor/Tests/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(TestViewModel testViewModel)
