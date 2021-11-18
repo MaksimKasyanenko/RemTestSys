aeb74bc171ae0aa8db3800f0b0d12c3044bfc6b7
Максим Касьяненко <maksim93012@gmail.com>
2021-11-18 11:58:34 +0200

    --

--- RemTestSys/Areas/Editor/Controllers/TestsController.cs
+++ RemTestSys/Areas/Editor/Controllers/TestsController.cs
@@ -26,7 +26,7 @@ namespace RemTestSys.Areas.Editor.Controllers
         // GET: Editor/Tests
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Tests.ToListAsync());
+            return View(await _context.Tests.Include(t=>t.MapParts).ToListAsync());
         }
 
         // GET: Editor/Tests/Details/5
@@ -77,66 +77,40 @@ namespace RemTestSys.Areas.Editor.Controllers
             return View(testViewModel);
         }
 
-        // GET: Editor/Tests/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edit(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var test = await _context.Tests.FindAsync(id);
+            var test = await _context.Tests.Where(t => t.Id == id).Include(t => t.MapParts).SingleOrDefaultAsync();
             if (test == null)
             {
                 return NotFound();
             }
-            return View(test);
+            return View(new TestViewModel {
+                Id=test.Id,
+                Name=test.Name,
+                Description=test.Description,
+                Duration=test.Duration,
+                MapParts=test.MapParts.ToArray()
+            });
         }
 
-        // POST: Editor/Tests/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,QuestionsCount,Duration")] Test test, int[] counts, double[] casts)
+        public async Task<IActionResult> Edit(TestViewModel testViewModel)
         {
-            if (id != test.Id)
-            {
-                return NotFound();
-            }
-
             if (ModelState.IsValid)
             {
-                List<Test.MapPart> map = new List<Test.MapPart>();
-                for (int i = 0; i < counts.Length || i < casts.Length; i++)
-                {
-                    map.Add(new Test.MapPart
-                    {
-                        TestId = test.Id,
-                        QuestionCount = counts[i],
-                        QuestionCast = casts[i]
-                    });
-                }
-                test.MapParts = map;
-                try
-                {
-                    _context.Update(test);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!TestExists(test.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+                Test test = await _context.Tests.Where(t=>t.Id == testViewModel.Id).Include(t=>t.MapParts).SingleOrDefaultAsync();
+                if (test == null) return NotFound();
+                _context.MapParts.RemoveRange(test.MapParts.ToArray());
+                await _context.SaveChangesAsync();
+                test.Name = testViewModel.Name;
+                test.Description = testViewModel.Description;
+                test.Duration = testViewModel.Duration;
+                test.MapParts = testViewModel.MapParts;
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(test);
+            return View(testViewModel);
         }
 
         // GET: Editor/Tests/Delete/5
