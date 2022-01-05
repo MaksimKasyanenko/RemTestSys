31bb91eb6c76f5d03081e85f607b9125a3e5878b
Maksim <kasyanenkomaks89@gmail.com>
2022-01-05 10:53:47 +0200

    --

---
+++ UnitTests/TestClassWithDb.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using RemTestSys.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTests
+{
+    public class TestClassWithDb
+    {
+        public TestClassWithDb(DbContextOptions<AppDbContext> options){
+            contextOptions = options;
+        }
+
+        private readonly DbContextOptions<AppDbContext> contextOptions;
+
+        protected void SeedDb(Action<AppDbContext> seed){
+            using (var context = new AppDbContext(contextOptions)){
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                seed.Invoke(context);
+                context.SaveChanges();
+            }
+        }
+    }
+}
