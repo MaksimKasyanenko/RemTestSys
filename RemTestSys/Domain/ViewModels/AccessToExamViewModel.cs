3c209c4911300b438ac5fb280e688cac2c380013
Maksim <kasyanenkomaks89@gmail.com>
2022-03-15 14:12:30 +0200

    Index in AccessesController has been refactored

--- RemTestSys/Domain/ViewModels/AccessToExamViewModel.cs
+++ RemTestSys/Domain/ViewModels/AccessToExamViewModel.cs
@@ -2,5 +2,10 @@ namespace RemTestSys.Domain.ViewModels;
 
 public class AccessToExamViewModel
 {
-    
+    public int Id{get;set;}
+    public string ExamName{get;set;}
+    public string TargetName{get;set;}
+    public bool ForAll{get;set;}
+    public bool ForGroup{get;set;}
+    public bool ForPerson{get;set;}
 }
